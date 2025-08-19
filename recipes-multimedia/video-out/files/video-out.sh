d4f657562587b1c7aaccb29ce63a9fa9a41baa6f
Code <code720220@gmail.com>
2025-08-19 11:58:22 +0300

    Dispay video stream on station
    Signed-off-by: Code <code720220@gmail.com>

---
+++ recipes-multimedia/video-out/files/video-out.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PORT=$1
+PORT=${PORT:-5600}
+
+echo "gst-launch running on port $PORT"
+
+gst-launch-1.0 -v udpsrc port=$PORT caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! \
+    rtph264depay ! avdec_h264 ! \
+    videoconvert ! \
+    fbdevsink
