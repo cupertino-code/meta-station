926a3f4352e6a2c4345a1f9794f6b93f381e8633
Code <code720220@gmail.com>
2025-09-29 10:35:13 +0300

    control: Use libmisc library
    Signed-off-by: Code <code720220@gmail.com>

--- recipes-extended/control/files/common.h
+++ recipes-extended/control/files/common.h
@@ -1,6 +1,9 @@
 #ifndef _COMMON_H_INCLUDED
 #define _COMMON_H_INCLUDED
 
+#include "utils.h"
+#include "shmem.h"
+
 #define VERSION "1.0"
 
 #define DEFAULT_PORT 5110
@@ -17,22 +20,10 @@ struct antenna_status {
     int power_status;
     int connect_status;
     int vbat;
+    struct shared_memory shm;
 };
 
 extern struct antenna_status antenna_status;
-
-void dump(uint8_t *buf, int len);
-uint8_t crc8_data(const uint8_t *data, int len);
-
-#define MAYBE_UNUSED    __attribute__((unused))
-#define PACKED __attribute__((packed))
-
-#ifndef CLOCK_MONOTONIC
-#define CLOCK_MONOTONIC 1
-#endif
-
-#define _unlikely(cond) __builtin_expect ((cond), 0)
-#define _likely(cond)   __builtin_expect ((cond), 1)
 
 #define LOG1(format, ...) do {              \
         if (verbose)                        \
